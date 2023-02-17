c283073fed2532e65499a4275341448446824ab1
vysagkarun <vysag@frugalscientific.com>
2023-02-17 20:34:00 +0530

    Initial Commit

---
+++ wacMTVysag/wacMTVysagApp.swift
@@ -0,0 +1,17 @@
+//
+//  wacMTVysagApp.swift
+//  wacMTVysag
+//
+//  Created by vysag k on 17/02/23.
+//
+
+import SwiftUI
+
+@main
+struct wacMTVysagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
